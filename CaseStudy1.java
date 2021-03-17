73ffb08a5639ce590f2fcf1d5f6ca28ffaebf77b
Yumul <ml.yumul0344@student.tsu.edu.ph>
2021-03-17 16:09:58 +0800

    Yumul Edit!

--- CaseStudy1.java
+++ CaseStudy1.java
@@ -6,19 +6,38 @@ Import javax.jws.WebService;
 public class CaseStudy1{ 
 
 //Topic is about Account Management(Admin Side)
-
+	private String Name;
+ 	private double Balance;
 	private int AuthenticationTicket;
  	private String Position;
 
 
 @WebMethod
-   public String getPosition(){
-       return Position;
+   	public String getName(){
+       	return Name;
    }
-   @WebMethod
-   public void setPostion(String pos){
-       Position = pos;
+@WebMethod
+   	public void setName(String n){
+       	Name = n;
    }
+
+@WebMethod
+ 	public double getBalance(){
+      	return Balance;
+   }
+@WebMethod
+  	public void setBalance(double bal){
+      	Balance = balance;
+   }
+
+@WebMethod
+	public String getPosition(){
+	return Position;
+}
+@WebMethod
+	public void setPostion(String pos){
+	Position = pos;
+}
 @WebMethod
 	Public int getAuthenticationTicket(){
 	Return AuthenticationTicket;
