fa5bbafea7792a2ed2ebb0cc8407a95a11cd34dd
Kur-Mir <h.hosenilla0849@student.tsu.edu.ph>
2021-03-17 15:49:06 +0800

    Soriano Edit

--- CaseStudy1.java
+++ CaseStudy1.java
@@ -1,15 +1,21 @@
 
+Import javax.jws.WebMethod;
+Import javax.jws.WebService;
 
-
-
+@WebService
 public class CaseStudy1{ 
 
-//Topic is about 
+//Topic is about Account Management(Admin Side)
 
+	Private int AuthenticationTicket;
 
-
-
-
-
+@WebMethod
+	Public int getAuthenticationTicket(){
+	Return AuthenticationTicket;
+}
+@WebMethod
+	Public void setAuthenticationTicket(int Auth){
+	AuthenticationTicket = Auth;
+}
 
 }
