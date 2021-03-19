7589687b67e5a08188dd0d7e90490f033255e76c
Kur-Mir <h.hosenilla0849@student.tsu.edu.ph>
2021-03-19 16:28:39 +0800

    HH1 Modified

--- ReCaseStudy1.java
+++ ReCaseStudy1.java
@@ -1,1 +1,28 @@
-import javax.jws.WebMethod
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class CaseStudy1 {
+     
+private String EmployeeID
+private String AccountID
+
+
+@WebMethod
+   	public String getEmployeeID(){
+  	return EmployeeID;
+}
+@WebMethod
+	public void setEmployeeID(String empid){
+	EmployeeID = empid;
+}
+@WebMethod
+	public String getAccountID(){
+	return AccountID;
+}
+@WebMethod
+	public void setAccountID(String accid){
+	AccountID = accid;
+   }
+}
+
