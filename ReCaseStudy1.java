bfa2cba84e0ebe2326ef87d85f6fbedb58023eee
MariaLouiseYumul <ml.yumul0344@student.tsu.edu.ph>
2021-03-19 17:26:46 +0800

    Yumul Modified

--- ReCaseStudy1.java
+++ ReCaseStudy1.java
@@ -3,11 +3,29 @@ import javax.jws.WebService;
 
 @WebService
 public class CaseStudy1 {
+private String Balance;
+private String Name;
 private String Posistion;
 private String AuthenticationTicket;     
 private String EmployeeID;
 private String AccountID;
 
+@WebMethod
+	public double getBalance(){
+	return Balance;
+}
+@WebMethod
+	public void setBalance(double bal){
+	Balance = bal;
+}
+@WebMethod
+	public String getName(){
+	return Name;
+}
+@WebMethod
+	public void setName(String n){
+	Name = n;
+}
 @WebMethod
 	public String getPosition(){
 	return Position;
